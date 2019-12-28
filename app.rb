e1564e70ae6e6e1dc016a0e92d96557b9fbab204
Student <student@Makerss-MacBook-Pro.local>
2019-12-28 15:12:49 +0000

    refactoring previous step to mvp

--- app.rb
+++ app.rb
@@ -1,18 +1,16 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    "Bookmark Manager"
-  end
+  # get '/' do
+  #   "Bookmark Manager"
+  # end
 
   get '/bookmarks' do
-    bookmarks = [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-    ]
-    bookmarks.join
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $0
+  
 end
