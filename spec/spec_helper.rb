0e37f7ad8a1730285a9ed84c267cd89dff9891a8
Student <student@Makerss-MacBook-Pro.local>
2019-12-28 17:48:14 +0000

    setting up a testing env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require_relative './setup_test_database'
+ENV['ENVIRONMENT'] = 'test'
+
 ENV['RACK_ENV'] = 'test'
 
 require(File.join(File.dirname(__FILE__), '..', 'app.rb'))
@@ -23,6 +26,7 @@ Capybara.app = BookmarkManager
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
